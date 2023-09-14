64746f6d67f00e75e279686f5e744dc19dbfc6ce
profe-ssor <87576786+profe-ssor@users.noreply.github.com>
2023-09-14 09:11:07 +0000

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-production-613a.up.railway.app']
 
 
 # Application definition
