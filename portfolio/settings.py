5344057e523983aa2d24d30a1d3875140bb9f359
profe-ssor <87576786+profe-ssor@users.noreply.github.com>
2023-09-15 07:02:24 +0000

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-=k^m@#z8o%$vf
 'RENDER' not in os.environ
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['collins-site.onrender.com']
 
