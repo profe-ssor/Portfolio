d343c2cf0c42990f326cb5e8a79c7255a17ffcdb
profe-ssor <kyerematengcollins93@gmail.com>
2023-09-15 01:47:46 +0000

    Render

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # HTTPS settings
 SESSION_COOKIE_SECURE = True
